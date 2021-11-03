349044abe0b7247cb566e2cc5636536ef384446a
italoneves <neves.italo@gmail.com>
2021-11-03 16:26:25 -0300

    Adicionando gráficos estaticos

--- frontend-res/src/App.tsx
+++ frontend-res/src/App.tsx
@@ -1,13 +1,31 @@
 import NavBar from 'componentes/NavBar';
 import Footer from 'componentes/Footer';
 import DataTable from 'componentes/DataTable';
-
+import BarChart from 'componentes/BarChart';
+import DonutChart from 'componentes/DonutChart';
 function App() {
   return (
     <>
     <NavBar/>
     <div className="container">
-      <h1 className="text-primary">Olá mundo</h1>
+      <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+      <div className= "row px-3">
+          <div className="col-sm-6">
+              <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
+              <BarChart/>
+
+          </div>
+          <div className="col-sm-6">
+              <h5 className="text-center text-secondary">Todas as vendas</h5>
+              <DonutChart/>
+
+          </div>
+
+      </div>
+
+      <div className="py-3"> <h2>Todas as vendas</h2> 
+      
+      </div>
       <DataTable/>
     </div>
     
