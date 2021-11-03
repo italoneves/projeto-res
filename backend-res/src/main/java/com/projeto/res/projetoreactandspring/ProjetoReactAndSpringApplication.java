949e4fd6424cdafd59732aa3424c573f86ca4eff
italoneves <neves.italo@gmail.com>
2021-11-02 22:13:25 -0300

    Projeto criado

---
+++ backend-res/src/main/java/com/projeto/res/projetoreactandspring/ProjetoReactAndSpringApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.res.projetoreactandspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoReactAndSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoReactAndSpringApplication.class, args);
+	}
+
+}
---
+++ backend-res/src/test/java/com/projeto/res/projetoreactandspring/ProjetoReactAndSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.res.projetoreactandspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoReactAndSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
